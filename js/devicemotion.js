1d14545899e51b89cd2c2a03692894de4e6bd9d5
chocho-1115 <849890769@qq.com>
2017-04-27 00:57:35 +0800

    edit

--- js/devicemotion.js
+++ js/devicemotion.js
@@ -1,51 +1,46 @@
-
-
-;window.DeviceMotionEventEasy = {
-	curTime : 100,//100,
-	last_update : 0,
-	shake : {
-		callback : function(){},//摇一摇的回调函数
-		B : false,//摇一摇的开关
+(function(){
+	var last_update = 0,
+	    last_x = null,
+	    last_y = null,
+	    last_z = null;
+	window.DME = window.DME || {
+		curTime : 100,//函数节流
+		B : false,
 		threshold : 5000,//阈值  控制摇动的力度  一般默认的5000  刚好差不多
-	},
-	variable : {
-			last_x : null,
-			last_y : null,
-			last_z : null  
-	},
-	direction : {
-		callback : function(){},//重力感应的回调函数
-		B : false,//重力感应的开关
-	},
-	deviceMotionHandler : function (eventData) {
-		var acceleration = eventData.accelerationIncludingGravity, 
-			curTime = (new Date()).getTime(),
-			this_ = window.DeviceMotionEventEasy;  
-		if ((curTime - this_.last_update) < this_.curTime)return false;//函数节流
-		//更新数据
-		var diffTime = curTime -this_.last_update;    
-		this_.last_update = curTime;        
- 
-		if(this_.shake.B){
-			var speed = Math.abs(acceleration.x +acceleration.y + acceleration.z - this_.variable.last_x - this_.variable.last_y - this_.variable.last_z) / diffTime * 10000;   
-			if (speed > this_.shake.threshold) {
-				this_.shake.callback();  
-			}
-			this_.variable.last_x = acceleration.x;    
-			this_.variable.last_y = acceleration.y;
-			this_.variable.last_z = acceleration.z;    
+		callback : null,
+		handler : function (eventData) {
+			var acceleration = eventData.accelerationIncludingGravity, 
+				curTime = (new Date()).getTime(),
+				self = window.DME;  
+			if ((curTime - last_update) < self.curTime)return false;
+			//更新数据
+			var diffTime = curTime - last_update;    
+			last_update = curTime;        
+	 
+			if(self.B){
+				var speed = Math.abs(acceleration.x +acceleration.y + acceleration.z - last_x - last_y - last_z) / diffTime * 10000;   
+				if (speed > self.threshold) {
+					self.callback&&self.callback();  
+				}
+				last_x = acceleration.x;    
+				last_y = acceleration.y;
+				last_z = acceleration.z;    
+			}  
 		}
-		if(this_.direction.B){
-			this_.direction.callback(acceleration.x,acceleration.y,acceleration.z); 
+	}
+}());
+
+
+window.DOE = {
+	callback : null,
+	B : false,
+	handler : function (eventData) {
+		var self = window.DOE;
+		if(self.B){
+			self.callback(eventData.beta,eventData.gamma,eventData.alpha);
 		}
-		
-		  
-		   
 	}
 };
-
-//;
-
 
 
 
